9402b7529a3ce1be1f2e9255129af4d9d4ac9df3
Vishavjeet Sharma <52985689+vishavjeet1999@users.noreply.github.com>
2024-04-05 19:32:26 +0000

    twitter

--- app/api/route.js
+++ app/api/route.js
@@ -1,38 +1,33 @@
 import { NextResponse } from "next/server";
-// require("dotenv").config({ path: __dirname + "/.env" });
-// const { TwitterApi } = require("twitter-api-v2");
+require("dotenv").config({ path: __dirname + "/.env" });
+const { TwitterApi } = require("twitter-api-v2");
 
-// const client = new TwitterApi({
-//   appKey: process.env.API_KEY,
-//   appSecret: process.env.API_SECRET,
-//   accessToken: process.env.ACCESS_TOKEN,
-//   accessSecret: process.env.ACCESS_SECRET,
-// });
+const client = new TwitterApi({
+    appKey: process.env.API_KEY,
+    appSecret: process.env.API_SECRET,
+    accessToken: process.env.ACCESS_TOKEN,
+    accessSecret: process.env.ACCESS_SECRET,
+});
 
-// const bearer = new TwitterApi(process.env.BEARER_TOKEN);
+const bearer = new TwitterApi(process.env.BEARER_TOKEN);
 
-// const twitterClient = client.readWrite;
-// const twitterBearer = bearer.readOnly;
-
-
-// const tweet = async () => {
-//   try {
-//     await twitterClient.v2.tweet("Hello world!");
-//   } catch (e) {
-//     console.log(e)
-//   }
-// }
-
-// tweet();
+const twitterClient = client.readWrite;
+const twitterBearer = bearer.readOnly;
 
 // To handle a GET request to /api
 export async function GET(request) {
-  // Do whatever you want
-  return NextResponse.json({ message: "Hello World" }, { status: 200 });
+    // Do whatever you want
+    try {
+        await twitterClient.v2.tweet("Hello world!");
+        return NextResponse.json({ message: "Hello World" }, { status: 200 });
+    } catch (e) {
+        console.log(e)
+        return NextResponse.json(e, { status: 200 });
+    }
 }
 
 // To handle a POST request to /api
 export async function POST(request) {
-  // Do whatever you want
-  return NextResponse.json({ message: "Hello World" }, { status: 200 });
+    // Do whatever you want
+    return NextResponse.json({ message: "Hello World" }, { status: 200 });
 }
